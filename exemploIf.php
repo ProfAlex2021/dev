922c7dac2d9d1fe6d4a68cf58a4ab3ebc43be4c7
ProfAlex2021 <80551988+ProfAlex2021@users.noreply.github.com>
2022-02-25 22:31:19 -0300

    Aula PW-02-25

---
+++ exemploIf.php
@@ -0,0 +1,13 @@
+<?php
+$favcolor = "red";
+
+if ($favcolor == "red") {
+    echo "Your favorite color is red!";
+} elseif ($favcolor == "green"){
+    echo "Your favorite color is green!";
+} elseif ($favcolor == "blue"){
+    echo "Your favorite color is blue!";
+} else {
+    echo "Your favorite color is neither red, green nor blue!";
+}
+?>
