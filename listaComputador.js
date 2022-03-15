39dd42eb958251cc4800ed9fafcafaf1f1a024f5
ProfAlex2021 <80551988+ProfAlex2021@users.noreply.github.com>
2022-03-15 20:08:13 -0300

    Finalizando a tabela

--- listaComputador.js
+++ listaComputador.js
@@ -1,12 +1,16 @@
 let dados;
 
-function carregarDados(){
+function carregarDados(cb){
     fetch("http://3tdsn2022.atwebpages.com/ListarComputador.php")
         .then(conteudo => conteudo.text())
-        .then(texto => dados = texto);
+        .then((texto) => {
+            dados = JSON.parse(texto);
+            cb();
+        });
 }
 
 function exibirTabela(){
+    console.log(dados);
     let tabela = '';
     for(let linha in dados){
         tabela += '<tr>';
@@ -16,4 +20,12 @@ function exibirTabela(){
         tabela += `<td>${dados[linha].dataAtualizacao}</td>`;
         tabela += '</tr>';
     }
+    document.getElementsByTagName('tbody')[0].innerHTML = tabela;
+    
 }
+
+function iniciar(){
+    carregarDados(exibirTabela)
+}
+
+window.onload = iniciar;
