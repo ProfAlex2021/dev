4f0484a132ad3e0f2f70ce468e58b075f8e83a23
ProfAlex2021 <80551988+ProfAlex2021@users.noreply.github.com>
2022-03-18 15:22:56 -0300

    Cadastro de Carro e Testes separados (Carro e Computador)

--- Carro.php
+++ Carro.php
@@ -8,6 +8,28 @@ include "Conexao.php";
         private string $ipvaData;
         private float $ipvaValor;
 
+        public function __construct(string $placa, int $ano, bool $ipvaPago, string $ipvaData, float $ipvaValor){
+            $this->placa = $placa;
+            $this->ano = $ano;
+            $this->ipvaPago = $ipvaPago;
+            $this->ipvaData = $ipvaData;
+            $this->ipvaValor = $ipvaValor;
+        }
+        public function Cadastrar(){
+            $conexao = new Conexao();
+            $sql = "INSERT INTO
+            Carro(placa, ano, ipvaPago, ipvaData, ipvaValor)
+            VALUES (:placa, :ano, :ipvaPago, :ipvaData, :ipvaValor)";
+            $pdo = $conexao->Conectar();
+            $preparo = $pdo->prepare($sql);
+            $preparo->bindParam(':ano', $this->ano);
+            $preparo->bindParam(':placa', $this->placa);
+            $preparo->bindParam(':ipvaData', $this->ipvaData);
+            $preparo->bindParam(':ipvaPago', $this->ipvaPago);
+            $preparo->bindParam(':ipvaValor', $this->ipvaValor);
+            $preparo->execute();
+        }
+
         public static function ListarTodos(){
             $conexao = new Conexao();
             $sql = "SELECT * FROM Carro";
